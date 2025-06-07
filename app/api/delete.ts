28634dc962b0b9d88d411a2fb67731510a94d8d0
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2025-06-08 08:33:06 +0900

    chore(part4): format and lint some api files

--- app/api/delete.ts
+++ app/api/delete.ts
@@ -1,48 +1,57 @@
-import { NextRequest, NextResponse } from "next/server"
-import { deletePlayerById, deleteUmpireById, deleteCourseById } from "@/app/lib/db/queries/queries"
+import {
+  deleteCourseById,
+  deletePlayerById,
+  deleteUmpireById,
+} from "@/app/lib/db/queries/queries"
 
-export async function deleteById(req: NextRequest, mode: string) {
+export async function deleteById(req: Request, mode: string) {
   // modeは"player"か"umpire"か"course"のいずれか
   const reqbody = await req.json()
   const { id } = reqbody
   try {
     if (Array.isArray(id)) {
       // idが配列の場合、全てのidを削除
-      const deletePromises = id.map(async (cid) => {
-        if (mode === "player") {
-          const result = await deletePlayerById(cid)
-          return result
-        } else if (mode === "umpire") {
-          const result = await deleteUmpireById(cid)
-          return result
-        } else if (mode === "course") {
-          const result = await deleteCourseById(cid)
-          return result
-        }
-      })
-      await Promise.all(deletePromises)
-      return NextResponse.json({ success: true, message: mode + " deleted successfully." }, { status: 200 })
-    } else {
-      if (mode === "player") {
-        const result = await deletePlayerById(id)
-        return NextResponse.json({ success: true, data: result }, { status: 200 })
-      } else if (mode === "umpire") {
-        const result = await deleteUmpireById(id)
-        return NextResponse.json({ success: true, data: result }, { status: 200 })
-      } else if (mode === "course") {
-        const result = await deleteCourseById(id)
-        return NextResponse.json({ success: true, data: result }, { status: 200 })
-      }
+      await Promise.all(
+        id.map(async (cid) => {
+          if (mode === "player") {
+            const result = await deletePlayerById(cid)
+            return result
+          }
+          if (mode === "umpire") {
+            const result = await deleteUmpireById(cid)
+            return result
+          }
+          if (mode === "course") {
+            const result = await deleteCourseById(cid)
+            return result
+          }
+        }),
+      )
+      return Response.json(
+        { success: true, message: `${mode} deleted successfully.` },
+        { status: 200 },
+      )
+    }
+    if (mode === "player") {
+      const result = await deletePlayerById(id)
+      return Response.json({ success: true, data: result }, { status: 200 })
+    }
+    if (mode === "umpire") {
+      const result = await deleteUmpireById(id)
+      return Response.json({ success: true, data: result }, { status: 200 })
+    }
+    if (mode === "course") {
+      const result = await deleteCourseById(id)
+      return Response.json({ success: true, data: result }, { status: 200 })
     }
   } catch (error) {
-    console.log("error: ", error)
-    return NextResponse.json(
+    return Response.json(
       {
         success: false,
         message: "An error occurred while creating the player.",
         error: error,
       },
-      { status: 500 }
+      { status: 500 },
     )
   }
 }
